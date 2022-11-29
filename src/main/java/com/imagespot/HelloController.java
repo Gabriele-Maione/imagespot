83a39005b07a2f25cc4b5608439fc78fda14dd1b
Gabriele Maione <gabrielemaione52@gmail.com>
2022-11-29 19:36:17 +0100

    First tests with Scene Builder

--- src/main/java/com/imagespot/HelloController.java
+++ src/main/java/com/imagespot/HelloController.java
@@ -5,10 +5,26 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label lbl;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Ciao Mondo");
+    protected void onSignInClick() {
+        lbl.setText(MD5("12345678"));
+    }
+
+    public String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (byte b : array) {
+                sb.append(Integer.toHexString((b & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        }
+        catch (java.security.NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
