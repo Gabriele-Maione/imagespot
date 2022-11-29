d6366f0065088f3c89654e3ec4ed69693b95ef31
Daniel Popov <daniyilpopov25@gmail.com>
2022-11-29 23:20:14 +0100

    added path to schema

--- src/main/java/com/imagespot/Connection/ConnectionManager.java
+++ src/main/java/com/imagespot/Connection/ConnectionManager.java
@@ -2,7 +2,7 @@ package com.imagespot.Connection;
 import java.sql.*;
 
 public class ConnectionManager {
-    private static final String url = "jdbc:postgresql://ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/d4uten7a2rimt9";
+    private static final String url = "jdbc:postgresql://ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/d4uten7a2rimt9?searchpath=Imagespot";
     private static final String driverName = "org.postgresql.Driver";
     private static final String username = "hlzqcandbxakhx";
     private static final String password = "a61a705efd67e4955bc54903f56ce61aec39a1d5474a353300ad691581762139";
