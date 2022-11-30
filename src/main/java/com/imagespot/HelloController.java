65f2927fa6ce50369933c7cbd8c0aad9700c7ca8
Gabriele Maione <gabrielemaione52@gmail.com>
2022-11-30 12:09:44 +0100

    Added account creation control

--- src/main/java/com/imagespot/HelloController.java
+++ src/main/java/com/imagespot/HelloController.java
@@ -4,11 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import static com.imagespot.Connection.ConnectionManager.getConnection;
 
@@ -23,22 +25,21 @@ public class HelloController {
     private TextField email;
     @FXML
     private TextField password;
+    @FXML
+    private Label lbl_error;
 
     //DB STUFF DA SISTEMARE
     @FXML
-    protected void signup() {
+    protected void signup() throws SQLException {
         Connection connection = getConnection();
         PreparedStatement statement;
-        try {
-            String sql = "INSERT INTO account(Username, Name, Email, password) VALUES(?, ?, ?, ?)";
-            statement = connection.prepareStatement(sql);
-            statement.setString(1, username.getText());
-            statement.setString(2, name.getText());
-            statement.setString(3, email.getText());
-            statement.setString(4, password.getText());
-            statement.execute();
-
-        } catch(Exception e) {e.printStackTrace();}
+        String sql = "INSERT INTO account(Username, Name, Email, password) VALUES(?, ?, ?, ?)";
+        statement = connection.prepareStatement(sql);
+        statement.setString(1, username.getText());
+        statement.setString(2, name.getText());
+        statement.setString(3, email.getText());
+        statement.setString(4, password.getText());
+        statement.execute();
     }
 
     @FXML
@@ -53,9 +54,9 @@ public class HelloController {
             stage.setScene(scene);
             btn_sign_in.getScene().getWindow().hide();
             stage.show();
-        } catch (IOException e) {
+        } catch (IOException | SQLException e) {
+            lbl_error.setVisible(true);
             throw new RuntimeException(e);
         }
-
     }
 }
